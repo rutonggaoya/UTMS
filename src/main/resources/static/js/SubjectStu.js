ecf11528c0b079b6656e5925ab9a5caa21b110a1
Sijar <1278824488@qq.com>
2019-06-24 16:31:37 +0800

    学生课程部分可以评价老师了  Merge branch 'Sijar'
    # Conflicts:
    #	src/main/resources/static/css/my1.css
    #	src/main/resources/templates/Student/dissertation.html
    #	src/main/resources/templates/Student/message.html
    #	src/main/resources/templates/Student/person.html
    #	src/main/resources/templates/Student/question.html
    #	src/main/resources/templates/Student/topic.html
    #	src/main/resources/templates/Teacher/TeacherEditInfo.html
    #	src/main/resources/templates/Teacher/TeacherPerson.html
    #	src/main/resources/templates/Teacher/TeacherSubject.html
    #	src/main/resources/templates/Teacher/TeacherThesis.html
    #	src/main/resources/templates/Teacher/Teachermessage.html
    #	src/main/resources/templates/Teacher/Teacherquestion.html

---
+++ src/main/resources/static/js/SubjectStu.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#stu_subj").click(function () {
+    //学生端查看课题
+    $('#Modal_stusubj').modal();
+    });
+    $("#stu_assess").click(function () {
+      //学生端查看课题
+      $('#Modal_stusubj2').modal();
+      });
+});
